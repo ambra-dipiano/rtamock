db822d5c51992b61319094c57844f2ce858b6ada
ambra-dipiano <ambra.dipiano@inaf.it>
2022-05-30 18:26:18 +0200

    utility script

---
+++ utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+from os import system
+from astropy.io import fits
+from sagsci.tools.fits import Fits
+
+def get_obj_pointing(fitsfile):
+    with fits.open(fitsfile) as h:
+        ra = h['EVENTS'].header['RA_OBJ']
+        dec = h['EVENTS'].header['DEC_OBJ']
+    return {'ra': ra, 'dec': dec}
+
+def get_obs_GTI(fitsfile):
+    with fits.open(fitsfile) as h:
+        GTI = h['GTI'].data[0]
+    return GTI
+
+def split_observation(fitsfile, nbins):
+    GTI = get_obs_GTI(fitsfile=fitsfile)
+    edges = np.linspace(GTI[0], GTI[1], num=nbins)
+    bins = []
+    for i in range(len(edges)-1):
+        selection = fitsfile.replace(".fits", f"_{edges[i]}_{edges[i+1]}.fits")
+        system(f'cp {fitsfile} {selection}')
+        bins.append(selection)
+    
+        f = Fits()
+        pointing = {'ra': f.get_dl3_hdr(selection)['RA_PNT'], 'dec': f.get_dl3_hdr(selection)['DEC_PNT']}
+        dl3 = f.get_dl3_data(selection)
+        dl3_selected = f.selection_cuts(dl3, pointing, trange=[edges[i], edges[i+1]])
+        f.set_dl3_data(selection, dl3_selected, GTI=[edges[i], edges[i+1]])
+
+    return bins
