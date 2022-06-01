06b8099b05f0c745121e035afa0f1a42c7ceeaa0
ambra-dipiano <ambra.dipiano@inaf.it>
2022-06-01 12:50:52 +0200

    join DF with multiple run and copyfiles option

--- plot_run_lightcurves.py
+++ plot_run_lightcurves.py
@@ -7,68 +7,82 @@
 # Ambra Di Piano <ambra.dipiano@inaf.it>
 # *******************************************************************************
 
+from os import system
 import yaml
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sagsci.tools.utils import get_absolute_path
+from sagsci.tools.utils import get_absolute_path, str2bool
 
 # get line command options
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--configfile", default="myconfig.yml", help="configuration YAML file")
+parser.add_argument("-cp", "--copyfiles", default="false", help="copy plot files in repository dir")
 args = parser.parse_args()
 
 # get YAML configuration
 configuration = open(args.configfile)
 config = yaml.load(configuration, Loader=yaml.FullLoader)
-datafile = get_absolute_path(config['plot']['data'])
 
 def plot_significance(data):
-    plt.errorbar(x=data['time'], y=data['sigma'], xerr=data['time_err'], yerr=data['sigma_err'], fmt='ro', barsabove=True, label='Li&Ma Significance')
+    plt.errorbar(x=data['time'], y=data['sigma'], xerr=data['time_err'], fmt='bo', barsabove=True, label='Li&Ma Significance')    
+    plt.ylabel(r'$\sigma$', fontsize=fs)
 
 def plot_excess(data):
-    plt.errorbar(x=data['time'], y=data['excess'], xerr=data['time_err'], yerr=data['excess_err'], fmt='ro', barsabove=True, label='excess counts')
+    plt.errorbar(x=data['time'], y=data['excess'], xerr=data['time_err'], yerr=data['excess_err'], fmt='bo', barsabove=True, label='excess counts')
+    plt.ylabel('counts', fontsize=fs)
 
 def plot_flux(data):
     plt.yscale('log')
-    plt.errorbar(x=data['time'], y=data['flux'], xerr=data['time_err'], yerr=data['flux_err'], fmt='ro', barsabove=True, label='integrated flux')
+    plt.errorbar(x=data['time'], y=data['flux'], xerr=data['time_err'], yerr=data['flux_err'], fmt='bo', barsabove=True, label='integrated flux')
+    plt.ylabel('F (ph/cm2/s)', fontsize=fs)
 
 def plot_background(data):
-    plt.errorbar(x=data['time'], y=data['off_counts'], xerr=data['time_err'], yerr=np.sqrt(data['off_counts']), fmt='ro', barsabove=True, label='background')
+    plt.errorbar(x=data['time'], y=data['off_counts'], xerr=data['time_err'], yerr=np.sqrt(data['off_counts']), fmt='bo', barsabove=True, label='background')
+    plt.ylabel('counts', fontsize=fs)
 
 def plot_counts(data):
-    plt.errorbar(x=data['time'], y=data['off_counts'], xerr=data['time_err'], yerr=np.sqrt(data['off_counts']), fmt='ro', barsabove=True, label='off counts')
-    plt.errorbar(x=data['time'], y=data['on_counts'], xerr=data['time_err'], yerr=np.sqrt(data['on_counts']), fmt='g^', barsabove=True, label='on counts')
+    plt.errorbar(x=data['time'], y=data['off_counts']/3, xerr=data['time_err'], yerr=np.sqrt(data['off_counts']/3), fmt='bo', barsabove=True, label=r'$\alpha\cdot$Noff')
+    plt.errorbar(x=data['time'], y=data['on_counts'], xerr=data['time_err'], yerr=np.sqrt(data['on_counts']), fmt='g^', barsabove=True, label='Non')
+    plt.ylabel('counts', fontsize=fs)
 
+data = pd.DataFrame()
+for f in config['plot']['data']:
+    datafile = get_absolute_path(f)
+    data.join(pd.read_csv(datafile, sep=' ', header=0))
 
-df = pd.read_csv(datafile, sep=' ', header=0)
+data['time'] = data['time'] - data['time'].min()
+
 which = config['plot']['which']
 fs = 16
 for w in which:
     fig = plt.figure(figsize=(10, 4))
-    plt.title(f"{config['source']} - {config['run']['runid']}", fontsize=fs)
+    plt.title(f"RUN{config['run']['runid']}: {config['source']} E({data['emin'][0]} - {data['emax'][0]}) TeV", fontsize=fs)
     plt.tick_params(axis='both', labelsize=fs)
-    plt.xlabel('MJD', fontsize=fs)
-    plt.ylabel(w, fontsize=fs)
+    plt.xlabel('time (s)', fontsize=fs)
 
     if w.lower() == 'background':
-        plot_background(data=df)
+        plot_background(data=data)
     elif w.lower() == 'counts':
-        plot_counts(data=df)
+        plot_counts(data=data)
     elif w.lower() == 'excess':
-        plot_excess(data=df)
+        plot_excess(data=data)
     elif w.lower() == 'significance':
-        plot_significance(data=df)
+        plot_significance(data=data)
     elif w.lower() == 'flux':
-        plot_flux(data=df)
+        plot_flux(data=data)
 
     plt.grid()
     plt.legend()
 
     plt.tight_layout()
-    outname = get_absolute_path(config['plot']['data']).replace('.txt', f'_{w}.png') 
+    outname = get_absolute_path(config['plot']['data']).replace('.csv', f'_{w}.png') 
     plt.savefig(outname)
+
+    if str2bool(args.copyfiles):
+        system(f'cp {outname} .')
+        system(f'cp {datafile} .')
 
 
 
