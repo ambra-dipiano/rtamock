8d2d27e9e22ec0d969f01e618c41061569f09dbe
ambra-dipiano <ambra.dipiano@inaf.it>
2022-06-07 18:19:18 +0200

    copy true

--- execute_run.sh
+++ execute_run.sh
@@ -3,4 +3,4 @@
 python prepare_dataset.py -f myconfig.yml
 python analysis_one_run.py -f myconfig.yml
 python collect_bins_results.py -f myconfig.yml
-python plot_run_lightcurves.py -f myconfig.yml -cp false
+python plot_run_lightcurves.py -f myconfig.yml -cp true
